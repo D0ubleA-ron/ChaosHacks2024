9891fa074e663647deba8e6d74762d715b5c51e7
D0ubleA-ron <adeo1@student.ubc.ca>
2024-03-02 14:00:02 -0800

    Auto stash before merge of "LocationApi" and "origin/main"

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import "./App.css";
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import styled from "styled-components";
@@ -97,3 +98,33 @@ export default function App() {
     </Grid>
   );
 }
+=======
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+    const [jsonData, setJsonData] = useState(null);
+
+    async function fetchData() {
+        try {
+            const response = await fetch("http://localhost:3001/search"); // Point to your local server
+            const data = await response.json();
+            console.log(data);
+            setJsonData(data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    const handleClick = () => {
+        fetchData();
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick}>Fetch Data</button>
+        </div>
+    );
+}
+export default App;
+>>>>>>> Stashed changes
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+<<<<<<< Updated upstream
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+=======
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
+>>>>>>> Stashed changes
