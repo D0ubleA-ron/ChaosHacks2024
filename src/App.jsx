16b026bd254f1d0bb473669c91fe9a0dca20e406
Maximus Joe <maximusjoe278@gmail.com>
2024-03-02 13:35:17 -0800

    Add state and page navigation to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
 import styled from "styled-components";
+import { useState } from "react";
 
 const StyledTextField = styled(TextField)`
   .MuiInput-underline:before {
@@ -22,30 +23,50 @@ const StyledTextField = styled(TextField)`
 `;
 
 export default function App() {
+  const [directionData, setDirectionData] = useState({});
+  const [page, setPage] = useState(0);
   const handleClick = () => {
     alert("YOU SUCK!");
+    setPage(page + 1);
   };
   return (
     <Grid container style={{ height: "100vh" }}>
-      <Grid item xs={12} height={"10%"} textAlign={"center"}>
-        <Typography color={"white"} variant="h3" marginTop={5}>
-          Moogle Gaps
-        </Typography>
-      </Grid>
-      <Grid item xs={12} height={"80%"}>
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          height="100%"
-        >
-          <StyledTextField
-            id="standard-basic"
-            label="Where do you want to drive?"
-            variant="standard"
-          />
-        </Box>
-      </Grid>
+      {page < 1 ? (
+        <>
+          <Grid item xs={12} height={"10%"} textAlign={"center"}>
+            <Typography color={"white"} variant="h3" marginTop={5}>
+              Moogle Gaps
+            </Typography>
+          </Grid>
+          <Grid item xs={12} height={"80%"}>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              height="100%"
+            >
+              <StyledTextField
+                label="Where do you want to drive?"
+                variant="standard"
+              />
+            </Box>
+          </Grid>
+        </>
+      ) : (
+        <Grid item xs={12} height={"90%"}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+          >
+            <Typography color={"white"} variant="h3">
+              next steps
+            </Typography>
+          </Box>
+        </Grid>
+      )}
+
       <Grid item xs={12} height={"10%"} textAlign={"center"}>
         <Button
           size="large"
