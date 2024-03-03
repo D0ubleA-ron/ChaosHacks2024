6dca6ef4228b3d629fc0cd7db3a6b19dad0c8469
ved-b <vballary@student.ubc.ca>
2024-03-02 17:25:56 -0800

    Auto updates user location

--- Maps/locationHelper.js
+++ Maps/locationHelper.js
@@ -1,22 +1,27 @@
+// locationHelper.js
 import { useState, useEffect } from "react";
 
 export default function useUserLocation() {
   const [userLocation, setUserLocation] = useState(null);
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setUserLocation({ latitude, longitude });
-        },
-        (error) => {
-          console.error("Error getting location:", error);
-        }
-      );
-    } else {
-      console.log("Geolocation is not supported by this browser");
-    }
+    const intervalId = setInterval(() => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setUserLocation({ latitude, longitude });
+          },
+          (error) => {
+            console.error("Error getting location:", error);
+          }
+        );
+      } else {
+        console.log("Geolocation is not supported by this browser");
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return userLocation;
